b46830fb0030c48a4c60f6d4063a8ebe27412bdd
M-Munier <marc@geomaps.de>
2020-02-14 18:07:40 +0100

    file Iteration

--- cpm3.py
+++ cpm3.py
@@ -5,11 +5,13 @@ import numpy as np
 import sys, os
 import re
 
-from calib import convert_to_pressure
+from calib import convert_to_pressure, import_HCL_data
 
 
 DATA_DIR = sys.argv[1] if len(sys.argv) > 1 else "data_UTF8"
-HCL_DATA_RE = re.compile("HCL_(\d+)_ms_(\d+)_deg_Pos(\d)_new.dat")
+HCL_DATA_RE = re.compile("HCL_(\\d+)_ms_(\\d+)_deg_Pos(\\d)_new.dat")
+
+measurements = []
 
 for file_entry in os.scandir(DATA_DIR):
 
@@ -20,7 +22,19 @@ for file_entry in os.scandir(DATA_DIR):
             groups = match.groups()
             assert len(groups) == 3
 
-            vel, AoA, pos = *groups
-            
+            measurements.append(groups)
 
-        
+Vel_set = set(([i[0] for i in measurements]))
+AoA_set = set(([i[1] for i in measurements]))
+Pos_set = set(([i[2] for i in measurements]))
+
+def calc_cpm3(tab):
+    
+    pass
+
+for vel in Vel_set: 
+    for pos in Pos_set:
+        for AoA in AoA_set:
+            filename = f"HCL_{vel}_{AoA}_deg_Pos{pos}_new.dat"
+            hcl_data = import_HCL_data(filename)
+            calc_cpm3(hcl_data)
